32a57cffded2fb41f888fb4a8d77597cc0d37a5b
FINNSEEFLY <matmak122@gmail.com>
2020-05-19 17:47:49 +0300

    Refactoring

--- Proxy.cs
+++ Proxy.cs
@@ -18,7 +18,12 @@ namespace MyProxy
     {
         private const string HOST = "127.0.0.1";
         private const string PATH_TO_FILES = "D:\\MyProxyFiles\\";
-        readonly private char[]  Separators = new char[] { '\r', '\n' };
+        private const string FORBIDDEN_MESSAGE = "HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\nContent-Length: 52\r\n\r\nThis website is blocked. Please accept our apologies";
+        private const string DATE_AND_TIME_FORMAT = "dd.MM.yyyy|HH:mm:ss";
+        private const string FILENAME_PROXY_LOG = "Log.txt";
+        private const string FILENAME_PROXY_ERR_LOG = "ErrLog.txt";
+        private const string FILENAME_BLACKLIST = "Blacklist.txt";
+        readonly private char[] Separators = new char[] { '\r', '\n' };
         private const int PROXY_PORT = 8080;
         private const int RECIVE_BUFFER_SIZE = 15360;
         private const int DEFAULT_SERVER_PORT = 80;
@@ -26,15 +31,16 @@ namespace MyProxy
         private bool isWorking;
         private List<string> BlackList;
         private TcpListener requestListener;
+
         public Proxy()
         {
             InitializeComponent();
+            BlackList = LoadBlackList();
+            requestListener = new TcpListener(IPAddress.Parse(HOST), PROXY_PORT);
         }
 
         protected override void OnStart(string[] args)
         {
-            BlackList = LoadBlackList();
-            requestListener = new TcpListener(IPAddress.Parse(HOST), PROXY_PORT);
             requestListener.Start();
             isWorking = true;
             AddInLog("MyProxy launched");
@@ -47,6 +53,26 @@ namespace MyProxy
             AddInLog("MyProxy is stopped");
         }
 
+        protected override void OnShutdown()
+        {
+            isWorking = false;
+            AddInLog("MyProxy is stopped by OS");
+            base.OnShutdown();
+        }
+
+        protected override void OnPause()
+        {
+            AddInLog("MyProxy is paused");
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            AddInLog("MyProxy has resumed working");
+            base.OnContinue();
+        }
+
+        // Слушаем входящие запросы браузера
         private void ListenRequests()
         {
             while (isWorking)
@@ -63,6 +89,7 @@ namespace MyProxy
             requestListener.Stop();
         }
 
+        // Получаем получаем данные от браузера
         private void ReceiveData(Socket socket)
         {
             NetworkStream browserStream = new NetworkStream(socket);
@@ -83,6 +110,7 @@ namespace MyProxy
             }
         }
 
+        // Обработка полученного запроса
         private void ProcessRequest(byte[] buffer, NetworkStream browserstream)
         {
             buffer = FixGET(buffer);
@@ -101,7 +129,7 @@ namespace MyProxy
                 else
                 {
                     serverStream.Write(buffer, 0, buffer.Length);
-                    
+
                     var responseBytes = new byte[32];
                     serverStream.Read(responseBytes, 0, responseBytes.Length);
                     browserstream.Write(responseBytes, 0, responseBytes.Length);
@@ -119,19 +147,22 @@ namespace MyProxy
             }
         }
 
+        // Извлекает из ответа от сервера код ответа
         private string GetResponseCode(byte[] response)
         {
             string[] head = Encoding.UTF8.GetString(response).Split(Separators);
             return head[0].Substring(head[0].IndexOf(" ") + 1);
         }
 
+        // Отправляет пользователю сообщение о запрете перехода
         private void ThrowForbidden(NetworkStream browserstream)
         {
-            byte[] data = Encoding.ASCII.GetBytes("HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\nContent-Length: 52\r\n\r\nThis website is blocked. Please accept our apologies");
+            byte[] data = Encoding.ASCII.GetBytes(FORBIDDEN_MESSAGE);
             browserstream.Write(data, 0, data.Length);
             return;
         }
 
+        // Проверка, есть ли адрес в черном списке
         private bool IsForbidden(string address)
         {
             if (BlackList.Contains(address.ToLower()))
@@ -141,7 +172,8 @@ namespace MyProxy
             else
                 return false;
         }
-      
+
+        // Получает TCPClient из Host
         private TcpClient GetTCPClient(string hostrecord)
         {
             string[] domainAndPort = hostrecord.Trim().Split(new char[] { ':' });
@@ -155,11 +187,13 @@ namespace MyProxy
             }
         }
 
+        // Разбивает ответ от сервера на строки
         private string[] ParseHTTP(byte[] buffer)
         {
             return Encoding.ASCII.GetString(buffer).Trim().Split(Separators);
         }
 
+        // Преобразует путь в относительный 
         private byte[] FixGET(byte[] buffer)
         {
             var strBuffer = Encoding.ASCII.GetString(buffer);
@@ -168,16 +202,17 @@ namespace MyProxy
             return buffer;
         }
 
+        // Добавление записи в основной лог
         private void AddInLog(string str)
         {
-            if (!File.Exists(PATH_TO_FILES+"Logs.txt"))
+            if (!File.Exists(PATH_TO_FILES + FILENAME_PROXY_LOG))
             {
-                File.Create(PATH_TO_FILES+"Logs.txt").Dispose();
+                File.Create(PATH_TO_FILES + FILENAME_PROXY_LOG).Dispose();
             }
             try
             {
-                string dateAndTime = "[" + DateTime.Now.ToString("dd.MM.yyyy|HH:mm:ss") + "] ";
-                File.AppendAllText(PATH_TO_FILES + "Logs.txt", dateAndTime + str+"\r\n");
+                string dateAndTime = "[" + DateTime.Now.ToString(DATE_AND_TIME_FORMAT) + "] ";
+                File.AppendAllText(PATH_TO_FILES + FILENAME_PROXY_LOG, dateAndTime + str + "\r\n");
             }
             catch (Exception e)
             {
@@ -185,28 +220,30 @@ namespace MyProxy
             };
         }
 
+        // Добавление записи в лог ошибок
         private void AddInErrLog(string str)
         {
-            if (!File.Exists(PATH_TO_FILES+"ProxyErrLog.txt"))
+            if (!File.Exists(PATH_TO_FILES + FILENAME_PROXY_ERR_LOG))
             {
-                File.Create(PATH_TO_FILES+ "ProxyErrLog.txt").Dispose();
+                File.Create(PATH_TO_FILES + FILENAME_PROXY_ERR_LOG).Dispose();
             }
             try
             {
-                string dateAndTime = "[" + DateTime.Now.ToString("dd.MM.yyyy|HH:mm:ss") + "] ";
-                File.AppendAllText(PATH_TO_FILES + "ProxyErrLog.txt", dateAndTime + str+"\r\n");
+                string dateAndTime = "[" + DateTime.Now.ToString(DATE_AND_TIME_FORMAT) + "] ";
+                File.AppendAllText(PATH_TO_FILES + FILENAME_PROXY_ERR_LOG, dateAndTime + str + "\r\n");
             }
             catch { };
         }
 
+        // Загрузить черный список
         private List<string> LoadBlackList()
         {
             List<string> blacklist = new List<string>();
-            if (File.Exists(PATH_TO_FILES+"blacklist.txt"))
+            if (File.Exists(PATH_TO_FILES + FILENAME_BLACKLIST))
             {
                 try
                 {
-                    var reader = new StreamReader(PATH_TO_FILES+"blacklist.txt");
+                    var reader = new StreamReader(PATH_TO_FILES + FILENAME_BLACKLIST);
                     while (!reader.EndOfStream)
                     {
                         blacklist.Add(reader.ReadLine());
@@ -220,7 +257,7 @@ namespace MyProxy
             }
             else
             {
-                File.Create(PATH_TO_FILES+"blacklist.txt").Dispose();
+                File.Create(PATH_TO_FILES + FILENAME_BLACKLIST).Dispose();
             }
             return blacklist;
         }
